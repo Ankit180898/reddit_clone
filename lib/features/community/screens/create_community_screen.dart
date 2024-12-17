2062789fb6c1403cd32adcf37af3133a052eef01
Ankit180898 <karan180898@gmail.com>
2024-12-17 23:46:51 +0530

    Implement community creation and enhance community list drawer
    - Added functionality to create a community in CreateCommunityScreen with a loading state.
    - Integrated a TextField for community name input and an ElevatedButton to trigger community creation.
    - Refactored CommunityListDrawer to use ConsumerStatefulWidget, allowing for dynamic loading of user communities.
    - Implemented error handling and loading states in the community list display.
    - Enhanced UI components for better user interaction and experience.

--- lib/features/community/screens/create_community_screen.dart
+++ lib/features/community/screens/create_community_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:reddit_clone/core/common/input_field.dart';
+import 'package:reddit_clone/core/common/loader.dart';
+import 'package:reddit_clone/features/community/controller/community_controller.dart';
+import 'package:reddit_clone/theme/palette.dart';
 
 class CreateCommunityScreen extends ConsumerStatefulWidget {
   const CreateCommunityScreen({super.key});
@@ -12,31 +14,65 @@ class CreateCommunityScreen extends ConsumerStatefulWidget {
 }
 
 class _CreateCommunityScreenState extends ConsumerState<CreateCommunityScreen> {
+  final communityNameController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    communityNameController.dispose();
+  }
+
+  void createCommunity() {
+    ref.read(communityControllerProvider.notifier).createCommunity(
+          communityNameController.text.trim(),
+          context,
+        );
+  }
+
   @override
   Widget build(BuildContext context) {
+    final isLoading = ref.watch(communityControllerProvider);
     return Scaffold(
       appBar: AppBar(
         title: const Text("Create Community"),
       ),
-      body: const Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          spacing: 16,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text("Community name"),
-            TextField(
-              decoration: InputDecoration(
-                  hintText: 'r/community_name',
-                  filled: true,
-                  border: InputBorder.none,
-                  contentPadding: EdgeInsets.all(18)),
-              maxLength: 21,
-              // controller: ,
-            )
-          ],
-        ),
-      ),
+      body: isLoading
+          ? const Loader()
+          : Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Column(
+                spacing: 16,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text("Community name"),
+                  TextField(
+                    controller: communityNameController,
+                    decoration: const InputDecoration(
+                        hintText: 'r/community_name',
+                        filled: true,
+                        border: InputBorder.none,
+                        contentPadding: EdgeInsets.all(18)),
+                    maxLength: 21,
+                    // controller: ,
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        foregroundColor: Palette.whiteColor,
+                        backgroundColor: Palette.blueColor,
+                        minimumSize: const Size(double.infinity, 50),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20))),
+                    onPressed: () {
+                      createCommunity();
+                    },
+                    child: const Text(
+                      "Create Community",
+                      style: TextStyle(fontSize: 18, color: Palette.whiteColor),
+                    ),
+                  )
+                ],
+              ),
+            ),
     );
   }
 }
--- lib/features/home/drawers/community_list_drawer.dart
+++ lib/features/home/drawers/community_list_drawer.dart
@@ -1,27 +1,67 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:reddit_clone/core/common/error_text.dart';
+import 'package:reddit_clone/core/common/loader.dart';
+import 'package:reddit_clone/features/auth/controller/auth_controller.dart';
+import 'package:reddit_clone/features/community/controller/community_controller.dart';
+import 'package:reddit_clone/features/community/repository/community_repository.dart';
 import 'package:routemaster/routemaster.dart';
 
-class CommunityListDrawer extends ConsumerWidget {
+class CommunityListDrawer extends ConsumerStatefulWidget {
   const CommunityListDrawer({super.key});
 
+  @override
+  ConsumerState<CommunityListDrawer> createState() =>
+      _CommunityListDrawerState();
+}
+
+class _CommunityListDrawerState extends ConsumerState<CommunityListDrawer> {
   void navigateToCreateCommunity(BuildContext context) {
     Routemaster.of(context).push('/create-community');
   }
 
+  void getUserCommunities() {
+    // Implement your logic to get user communities here
+    ref.watch(communityControllerProvider.notifier).getUserCommunities();
+  }
+
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
+    // You can use ref to read providers if needed
+    // final userCommunities = ref.watch(userCommunitiesProvider);
+
     return Drawer(
       child: SafeArea(
-          child: Column(
-        children: [
-          ListTile(
-            onTap: () => navigateToCreateCommunity(context),
-            leading: const Icon(Icons.add),
-            title: const Text("Create a community"),
-          )
-        ],
-      )),
+        child: Column(
+          children: [
+            ListTile(
+              onTap: () => navigateToCreateCommunity(context),
+              leading: const Icon(Icons.add),
+              title: const Text("Create a community"),
+            ),
+            ref.watch(userCommunityProvider).when(
+                  data: (communities) => Expanded(
+                    child: ListView.builder(
+                      itemCount: communities.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        var community = communities[index];
+                        return ListTile(
+                          leading: CircleAvatar(
+                            backgroundImage: NetworkImage(community.avatar),
+                          ),
+                          title: Text('r/${community.name}'),
+                          onTap: () => null,
+                        );
+                      },
+                    ),
+                  ),
+                  error: (error, StackTrace) =>
+                      ErrorText(error: error.toString()),
+                  loading: () => const Loader(),
+                )
+          ],
+        ),
+      ),
     );
   }
 }
